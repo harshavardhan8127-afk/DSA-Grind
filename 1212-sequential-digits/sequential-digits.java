e539562c35f88066466bc8c5ad317f1b24a820cb
harshavardhan8127-afk <harshavardhan8127@gmail.com>
2026-07-13 12:00:44 +0530

    Time: 1 ms (28.06%) | Memory: 42.1 MB (87.35%) - LeetSync

--- 1212-sequential-digits/sequential-digits.java
+++ 1212-sequential-digits/sequential-digits.java
@@ -1,29 +1,40 @@
 class Solution {
     public List<Integer> sequentialDigits(int low, int high) {
 
-        String[] subs = new String[36];
+        // String[] subs = new String[36];
+        List<Integer> res = new ArrayList<>();
         String s = "123456789";
-        int id = 0;
+        // int id = 0;
 
-        for(int i=0;i<=s.length();i++){
+        // for(int i=0;i<=s.length();i++){
+        //     for(int j=i;j<=s.length();j++){
+        //         if(j - i + 1 > 2){
+        //             subs[id] = s.substring(i,j);
+        //             id++;
+        //         }
+        //     }
+        // }
+        // Arrays.sort(subs,Comparator.comparingInt(String::length));
+        // for(int i=0;i<subs.length;i++){
+        //     int n = Integer.parseInt(subs[i]);
+
+        //     if(n >= low && n <= high){
+        //         res.add(n);
+        //     }
+        // }
+
+         for(int i=0;i<=s.length();i++){
             for(int j=i;j<=s.length();j++){
                 if(j - i + 1 > 2){
-                    subs[id] = s.substring(i,j);
-                    id++;
+                    String sub = s.substring(i,j);
+                    int n = Integer.parseInt(sub);
+                    if(n >= low && n <= high){
+                        res.add(n);
+                    }
                 }
             }
         }
-        Arrays.sort(subs,Comparator.comparingInt(String::length));
-
-        List<Integer> res = new ArrayList<>();
-
-        for(int i=0;i<subs.length;i++){
-            int n = Integer.parseInt(subs[i]);
-
-            if(n >= low && n <= high){
-                res.add(n);
-            }
-        }
+        Collections.sort(res);
 
         return res;
         
