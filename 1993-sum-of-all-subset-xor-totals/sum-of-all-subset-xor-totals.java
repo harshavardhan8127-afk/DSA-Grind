b63456b3e16b46dbec2db9a73c228d306ed91739
harshavardhan8127-afk <harshavardhan8127@gmail.com>
2026-07-24 18:35:55 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (28.52%) - LeetSync

---
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int subsetXORSum(int[] nums) {
+
+        int xor = 0;
+
+        return helper(nums,0,0);
+    }
+    static int helper(int[] nums,int index,int xor){
+
+        if(index == nums.length){
+            return xor;
+        }
+
+        //Taking it
+        int left = helper(nums,index+1,xor ^ nums[index]);
+
+        //Not taking it
+        int right = helper(nums,index+1,xor);
+
+        return left+right;
+    }
+}
