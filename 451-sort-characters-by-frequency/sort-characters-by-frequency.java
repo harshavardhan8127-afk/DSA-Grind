cfd8f46ef33ed55858963a0f7940dfed9cc32d19
harshavardhan8127-afk <harshavardhan8127@gmail.com>
2026-08-01 18:10:29 +0530

    Time: 16 ms (36.47%) | Memory: 46.6 MB (52.58%) - LeetSync

---
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String frequencySort(String s) {
+
+       HashMap<Character,Integer> map = new HashMap<>();
+
+       for(char ch : s.toCharArray()){
+        map.put(ch,map.getOrDefault(ch,0)+1);
+       }
+
+       List<Character> list = new ArrayList<>(map.keySet());
+       // this list.sort give the which character should come before and after by its comparing value;
+       list.sort((a,b) -> map.get(b) - map.get(a));
+
+       StringBuilder sb = new StringBuilder();
+
+       for(char ch : list){
+        for(int i=0;i<map.get(ch); i++){
+            sb.append(ch);
+        }
+       }
+
+        return sb.toString();
+    }
+}
