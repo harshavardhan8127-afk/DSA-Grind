635cefc0b00dff8b76a255e47bed19ed99edbfeb
harshavardhan8127-afk <harshavardhan8127@gmail.com>
2026-08-03 11:12:20 +0530

    Time: 0 ms (100.00%) | Memory: 53.2 MB (8.96%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -2,10 +2,24 @@ class Solution {
     public int singleNonDuplicate(int[] nums) {
 
         
-        int xor = 0;
-        for(int i=0;i<nums.length;i++){
-            xor ^= nums[i];
+        // int xor = 0;
+        // for(int i=0;i<nums.length;i++){
+        //     xor ^= nums[i];
+        // }
+        // return xor;
+        // This solutins works buts it need the TC of O(nlogn) which is binary search
+
+        int l = 0;
+        int r = nums.length - 1;
+
+        while(l<r){
+            int mid = l + (r-l)/2;
+            if(mid%2 == 0 && nums[mid] == nums[mid+1] || (mid %2 == 1 && nums[mid] == nums[mid - 1])){
+                l = mid + 1;
+            }
+            else r = mid;
+            
         }
-        return xor;
+        return nums[l];
     }
 }
