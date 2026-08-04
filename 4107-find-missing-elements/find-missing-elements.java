ef45d99b94e8c6c493c1a314107a85a4b4f8c25d
harshavardhan8127-afk <harshavardhan8127@gmail.com>
2026-08-04 10:08:27 +0530

    Time: 6 ms (39.24%) | Memory: 47.5 MB (5.33%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.java
+++ 4107-find-missing-elements/find-missing-elements.java
@@ -3,38 +3,38 @@ class Solution {
     
     //int[] nums = {1,2,3,1,6,7};
 
-        Arrays.sort(nums);
-        List<Integer> list = Arrays.stream(nums).boxed().toList();
-        List<Integer> res = new ArrayList<>();
+        // Arrays.sort(nums);
+        // List<Integer> list = Arrays.stream(nums).boxed().toList();
+        // List<Integer> res = new ArrayList<>();
 
-        int min = nums[0];
-        int max = nums[nums.length - 1];
-        for(int i=min;i<max;i++){
-            res.add(i);
+        // int min = nums[0];
+        // int max = nums[nums.length - 1];
+        // for(int i=min;i<max;i++){
+        //     res.add(i);
+        // }
+
+        // res.removeAll(list);
+
+        // return res;
+    
+    
+    
+    
+    
+       Arrays.sort(nums);
+       List<Integer> list=new ArrayList<>();
+    //    for(int i=1;i<nums.length;i++){
+    //if(nums.length==1) return list;
+    //     int j
+        int i=1;
+        while(i<=nums.length-1){
+            while(nums[i-1]+1!=nums[i]&&i<=nums.length-1){
+            list.add(nums[i-1]+1);
+            nums[i-1]=nums[i-1]+1;
         }
-
-        res.removeAll(list);
-
-        return res;
-    
-    
-    
-    
-    
-    //    Arrays.sort(nums);
-    //    List<Integer> list=new ArrayList<>();
-    // //    for(int i=1;i<nums.length;i++){
-    // //if(nums.length==1) return list;
-    // //     int j
-    //     int i=1;
-    //     while(i<=nums.length-1){
-    //         while(nums[i-1]+1!=nums[i]&&i<=nums.length-1){
-    //         list.add(nums[i-1]+1);
-    //         nums[i-1]=nums[i-1]+1;
-    //     }
-    //     i++;
-    //    }
-    //    return list;
+        i++;
+       }
+       return list;
 
 
 
