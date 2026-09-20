a1e265976516596757b7801f7b9d228a4294a1b6
harshavardhan8127-afk <harshavardhan8127@gmail.com>
2026-09-20 15:34:53 +0530

    Time: 1 ms (100.00%) | Memory: 44.3 MB (18.31%) - LeetSync

--- 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.java
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.java
@@ -1,16 +1,22 @@
 class Solution {
     public int reverseDegree(String s) {
 
-     HashMap<Character, Integer> map = new HashMap<>();
+    //  HashMap<Character, Integer> map = new HashMap<>();
 
-     for(char ch = 'a'; ch <= 'z'; ch++){
-        map.put(ch, 'z' - ch + 1);
-     }
+    //  for(char ch = 'a'; ch <= 'z'; ch++){
+    //     map.put(ch, 'z' - ch + 1);
+    //  }
 
-     int val = 0;
-     for(int i=0;i<s.length();i++){
-     val += (i+1) * map.get(s.charAt(i));
-     }
+    //  int val = 0;
+    //  for(int i=0;i<s.length();i++){
+    //  val += (i+1) * map.get(s.charAt(i));
+    //  }
+    int val =0;
+    for(int i=0;i<s.length();i++){
+        char ch = s.charAt(i);
+        int p = ('z'- ch + 1) * (i+1);
+        val += p;
+    }
       return val;  
     }
 }
